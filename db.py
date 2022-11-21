9ca8bfc59c8c57fb75301f6eb655d8bc91726073
201711008 <kymyy2675@gmail.com>
2022-11-21 17:22:18 +0900

    예외처리 수정

--- db.py
+++ db.py
@@ -1,23 +1,25 @@
 import pymysql.cursors
 import csv
+import os
 
 #mysql 연결
 class mysql_application():
 
     def __init__(self):
-        self.conn = pymysql.connect(
+        
+        try:
+            self.conn = pymysql.connect(
             host='54.180.1.233',
             port = 3306,
             user='kym2675',
             password='dbals3319',
             db='moviedb',
             charset='utf8')
-        if(self.conn):
             print("DB CONNECTED")
-            
-        # sql2 = """drop table movie"""
+        except :
+            print("failed")
+
         self.curs=self.conn.cursor()
-        # self.curs.execute(sql2)
     
         sql = """create table movie (
             id           char(3), 
@@ -31,12 +33,18 @@ class mysql_application():
             grade        varchar (50),
             primary key (id));
     """
-        # self.curs.execute(sql)
-        self.conn.commit()
+        try:
+            print("테이블이 없습니다. 새로 생성합니다.")
+            self.curs.execute(sql)
+            self.conn.commit
+        except:
+            print("테이블이 존재합니다. 기존 테이블을 사용합니다.")
+            self.conn.commit()
         
     def insert_data(self):
+        pwd = os.getcwd()
         sql = """INSERT INTO movie values(%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-        with open("./movie_data.TXT",'r',encoding='cp949') as f:
+        with open(pwd+"/movie_data.TXT",'r',encoding='cp949') as f:
             read_data = f.readlines()
 
         for data in read_data:
@@ -50,23 +58,32 @@ class mysql_application():
         return 
           
     def search_title(self,str):
+      
         sql = "select * from movie where title like %s"
-        self.curs.execute(sql,'%'+str+'%')
-        for f in self.curs.fetchone():
-            print(f)
+        try:
+            self.curs.execute(sql,'%'+str+'%')
+            for f in self.curs.fetchall():
+                print(f)
+        except:
+            print("검색 결과가 없습니다.")
         
     def search_total_num(self,total_num):
         sql = """select * from movie where totalnum > %s"""
-        self.curs.execute(sql,total_num)
-        for f in self.curs.fetchall():
-            for r in f:
-                print(r)
+        try:
+            self.curs.execute(sql,total_num)
+            for f in self.curs.fetchall():
+                print(f)
+        except:
+            print("검색 결과가 없습니다.")
 
     def search_date(self,start_date,end_date):
         sql="""select * from movie where between %s and %s"""
-        self.curs.execute(sql,start_date,end_date)
-        for f in self.curs.fetchone():
-            print(f)
+        try:
+            self.curs.execute(sql,start_date,end_date)
+            for f in self.curs.fetchall():
+                print(f)
+        except:
+            print("검색 결과가 없습니다.")
         
     
     def end_application(self):
